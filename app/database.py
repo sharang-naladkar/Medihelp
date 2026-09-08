b1285bf949893ab4eab019560d95dd355d062d6a
Sharang Naladkar <girisharang.1156@gmail.com>
2026-09-08 09:04:54 +0530

    Initialize SQLAlchemy with psycopg v3 URL

--- app/database.py
+++ app/database.py
@@ -5,5 +5,5 @@ from .config import DatabaseSettings
 class Base(DeclarativeBase):
     pass
 
-engine = create_engine(DatabaseSettings().database_url, pool_pre_ping=True)
+engine = create_engine(DatabaseSettings().sqlalchemy_url(), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
