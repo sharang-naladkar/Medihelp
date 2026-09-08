9fb7d7477f2c0e6087ab9446bf02ffdb74442c0b
Sharang Naladkar <girisharang.1156@gmail.com>
2026-09-08 09:18:22 +0530

    Fix pymavlink MISSION_ITEM_INT constructor

--- app/mission.py
+++ app/mission.py
@@ -12,7 +12,7 @@ def generate_mission(home_lat: float, home_lng: float, target_lat: float, target
     commands = [mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH]
     items = []
     for seq, ((lat, lng, alt), command) in enumerate(zip(points, commands)):
-        message = mavutil.mavlink.MAVLink_mission_item_int_message(1, 1, seq, MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, command, int(seq == 0), 1, 0, 0, 0, 0, int(lat * 1e7), int(lng * 1e7), alt, 0)
+        message = mavutil.mavlink.MAVLink_mission_item_int_message(1, 1, seq, MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, command, int(seq == 0), 1, 0, 0, 0, 0, int(lat * 1e7), int(lng * 1e7), alt)
         raw = {field: getattr(message, field) for field in message.fieldnames}
         raw["message_type"] = "MISSION_ITEM_INT"
         items.append(raw)
